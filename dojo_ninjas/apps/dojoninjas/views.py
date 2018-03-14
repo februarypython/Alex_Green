5edc24a8e315b7faed03abab538369304e3b44d2
Alex Green <alexkenta@gmail.com>
2018-03-14 13:58:38 -0700

    Django -- Dojo Ninjas

---
+++ dojo_ninjas/apps/dojoninjas/views.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, HttpResponse
+
+def index(request):
+    return HttpResponse("test")
+
+# Create your views here.
